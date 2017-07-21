254d1c6ca07f301af8e5cb704235c3e793481dcd
kermian <kermian@naver.com>
2017-07-21 14:54:48 +0900

    while 문

---
+++ C#/170721/while_loop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop
+{
+    class Program
+    {
+        // 1, 3, 5, 7, 9, 11, 13, 15
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("값 : " + i);
+                i += 2;
+            }
+            while (i <= 15);
+
+
+            /*
+            if(  )
+            {    // 메모리 영역 가변으로 => 1개의 기능을 수행
+                Console ~~;
+            }
+            if(  )
+                Console ~~;     // 처리영역(기능수행영역 1줄)
+            */
+
+         
+            
+            //while(조건) >....   true false
+            /*
+            while (true)    // c : true - bool - 1(int) c# : true - bool
+            {
+
+            }
+            */
+
+
+            // do
+            // {
+            //  }
+            // while();
+
+        }
+    }
+}
