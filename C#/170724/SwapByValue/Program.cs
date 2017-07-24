386802b253b27f2aaae1428b9a969573dac39b95
kermian <kermian@naver.com>
2017-07-24 14:22:24 +0900

    매개변수

---
+++ C#/170724/SwapByValue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapByValue
+{
+    class Program
+    {
+        public static void Swap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine("x:{0}, y:{1}", x, y);
+
+            Swap(x, y);
+
+            Console.WriteLine("x:{0}, y:{1}", x, y);
+        }
+    }
+}
