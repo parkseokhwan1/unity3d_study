eb3f7532588fcf203052c37c148b694463664b5b
kermian <kermian@naver.com>
2017-07-24 17:48:55 +0900

    재귀함수 - 거듭제곱

---
+++ homework/Recursion/ex01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01
+{
+    class Program
+    {
+        static int power(int a, int num)
+        {
+            if (num == 0)
+                return 1;
+
+            return a * power(a, num - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("거듭제곱 결과 : {0}", power(5, 3));
+        }
+    }
+}
