078cf89bca9fabd0c12bf24514a2daae5c285964
kermian <kermian@naver.com>
2017-07-24 17:04:40 +0900

    매개변수 데이터 할당

---
+++ C#/170724/NamedParameter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameter
+{
+    class Program
+    {
+        static void PrintProfile(string name, string phone)
+        {
+            Console.WriteLine("Name : {0}, Phone : {1}", name, phone);
+        }
+
+        static void Main(string[] args)
+        {
+            PrintProfile(name: "박찬호", phone: "010-123-1234");
+            PrintProfile(phone: "010-987-9876", name: "박지성");
+            PrintProfile("박세리", phone: "010-222-2222");
+            PrintProfile("박상현", phone: "010-567-5678");
+        }
+    }
+}
