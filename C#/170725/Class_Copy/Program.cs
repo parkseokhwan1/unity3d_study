7094e3d7620eba6820c8aefbd050f675a1fc7f69
parkseokhwan1 <kermian@naver.com>
2017-07-26 02:19:19 +0900

    얕은복사, 깊은복사 설명

--- C#/170725/Class_Copy/Program.cs
+++ C#/170725/Class_Copy/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+  
 class MyClass
 {
     public int data1;       // 생성자 X
--- C#/170725/Class_Copy02/Program.cs
+++ C#/170725/Class_Copy02/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+  
 class BookData
 {
     public string m_title;
