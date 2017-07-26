7b72d4ff5258635913aa5ac2761ac0493e9f2e8b
kermian <kermian@naver.com>
2017-07-26 14:03:55 +0900

    This() 생성자

---
+++ C#/170726/ThisConstructor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisConstructor
+{
+    class MyClass
+    {
+        int a, b, c;
+
+        public MyClass()
+        {
+            this.a = 5425;
+            Console.WriteLine("MyClass()");
+        }
+
+        public MyClass(int b) : this()
+        {
+            this.b = b;
+            Console.WriteLine("MyClass({0})", b);
+        }
+
+        public MyClass(int b, int c) : this(b)
+        {
+            this.c = c;
+            Console.WriteLine("MyClass({0}. {1})", b, c);
+        }
+
+        public void PrintFields()
+        {
+            Console.WriteLine("a:{0}, b{1}, c:{2}", a, b, c);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass a = new MyClass();
+            a.PrintFields();
+            Console.WriteLine();
+
+            MyClass b = new MyClass(1);
+            b.PrintFields();
+            Console.WriteLine();
+
+            MyClass c = new MyClass(10, 20);
+            c.PrintFields();
+        }
+    }
+}
