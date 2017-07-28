adc58cfbfb7f0194eb3dcff5ed03b630a3afe0d4
kermian <kermian@naver.com>
2017-07-28 17:30:38 +0900

    오버라이딩 예제

---
+++ C#/170728/class_vm_ex02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class parent
+{
+    public virtual void name()
+    {
+        Console.WriteLine("부모");
+    }
+}
+
+class child : parent
+{
+    public override void name()     // 자식 : 재정의 의미 , 파생클래스 입장 :  virtual 의미
+    {
+        Console.WriteLine("자식");
+    }
+}
+
+class grandson : child
+{
+    public override void name()
+    {
+        Console.WriteLine("손자");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        parent obj1 = new parent();
+        child obj2 = new child();
+        grandson obj3 = new grandson();
+
+        obj1.name();
+        obj2.name();
+        obj3.name();
+    }
+}
+
