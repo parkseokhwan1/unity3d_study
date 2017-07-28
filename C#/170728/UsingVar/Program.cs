6cd538e1c242fb7b4f3312bce9e4129df51e6114
kermian <kermian@naver.com>
2017-07-28 17:08:45 +0900

    var 키워드

---
+++ C#/170728/UsingVar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 20;
+            Console.WriteLine("Type : {0}, Value : {1}", a.GetType(), a);
+
+            var b = 3.1414213;
+            Console.WriteLine("Type : {0}, Value : {1}", b.GetType(), b);
+
+            var c = "Hello, World!";
+            Console.WriteLine("Type : {0}, Value : {1}", c.GetType(), c);
+
+            var d = new int[] { 10, 20, 30 };
+            Console.Write("Type : {0} , Value : ", d.GetType());
+            foreach (var e in d)
+                Console.Write("{0} ", e);
+
+            Console.WriteLine();
+        }
+    }
+}
