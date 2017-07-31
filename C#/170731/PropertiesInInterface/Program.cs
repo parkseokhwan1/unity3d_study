a18d2b4bd139c807a2507b9f14516058ae2593de
kermian <kermian@naver.com>
2017-07-31 17:10:45 +0900

    인터페이스 프로퍼티

---
+++ C#/170731/PropertiesInInterface/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesInInterface
+{
+    interface INamedValue
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+        string Value
+        {
+            get;
+            set;
+        }
+    }
+
+    class NamedValue : INamedValue
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Value
+        {
+            get;
+            set;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NamedValue name = new NamedValue()
+            { Name = "이름", Value = "박상현" };
+
+            NamedValue height = new NamedValue()
+            { Name = "키", Value = "177cm" };
+
+            NamedValue weight = new NamedValue()
+            { Name = "몸무게", Value = "90Kg" };
+
+            Console.WriteLine("{0} : {1}", name.Name, name.Value);
+            Console.WriteLine("{0} : {1}", height.Name, height.Value);
+            Console.WriteLine("{0} : {1}", weight.Name, weight.Value);
+        }
+    }
+}
