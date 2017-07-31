6967142f5b99d476f801e8929aee15243667dd19
kermian <kermian@naver.com>
2017-07-31 17:34:29 +0900

    컬렉션

---
+++ C#/170731/Collection/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+// 같은 자료형의 집합 : 1개이상의 => collection
+// Arraylist = list 객체
+// HashTable => Dictionay (key-value 객체)
+// stack, Queue;
+// using System.Collections
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = new ArrayList();  // 생성 초기화
+                                            // 데이터 없는 최소한의 공간
+            // a.Add, a.Remove, a.Insert, a.RemoveAt, a.IndexOf, a.Clear, a.Count
+
+
+        }
+    }
+}
