595cdbd287ef6d3abc62dcd7cf36f6a832aebce5
parkseokhwan1 <kermian@naver.com>
2017-08-17 17:54:09 +0900

    ,,

---
+++ Unity/workspace/Run_Test/Assets/Script/Block_Loop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block_Loop : MonoBehaviour {
+    public float Block_Speed = 6;
+
+    public GameObject[] Block;      // 프리팹받는 배열
+
+    public GameObject A_Zone;
+    public GameObject B_Zone;
+    //public GameObject Block;    // 프리팹을 통해서 계속만들어지는 객체
+
+	void Update () {
+        move();
+	}
+
+    void move()
+    {
+        A_Zone.transform.Translate(Vector3.left * Block_Speed * Time.deltaTime);
+        B_Zone.transform.Translate(Vector3.left * Block_Speed * Time.deltaTime);
+        if(B_Zone.transform.position.x <= 0)
+        {
+            Destroy(A_Zone);
+            A_Zone = B_Zone;
+            MakeBlock();
+        }
+    }
+
+    void MakeBlock()
+    {
+        int numdata = Random.Range(0, Block.Length);
+        B_Zone = Instantiate(Block[numdata], new Vector3(30, -10, 0), this.transform.rotation);
+    }
+}
---
+++ Unity/workspace/Run_Test/Assets/Script/Player_control.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum PlayerState { Run, Jump, D_Jump, Death }
+
+public class Player_control : MonoBehaviour {
+    public PlayerState Ps;
+    public float Jump_power = 600f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        this.GetComponent<Rigidbody>().WakeUp();    // 물리성질 한번 충돌체크한 후에
+        // 자동적으로 슬립형태로 빠진다( 충돌처리횟수를 자동으로 최적화)
+        // 강제로 슬립형태를 깨우는 방식으로 물리 처리 => 누락되는 충돌체크가 사라짐.
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            if (this.Ps == PlayerState.Jump) { D_Jump(); }
+            if  (this.Ps == PlayerState.Run) { Jump();   }
+        }	
+	}
+
+    void Jump()
+    {
+        Ps = PlayerState.Jump;
+        this.GetComponent<Rigidbody>().AddForce(Vector3.up * Jump_power);
+    }
+
+    void D_Jump()
+    {
+        Ps = PlayerState.D_Jump;
+        this.GetComponent<Rigidbody>().AddForce(Vector3.up * Jump_power);
+    }
+
+    void Death()
+    {
+        Ps = PlayerState.Death;
+    }
+
+    void Run()
+    {
+        Ps = PlayerState.Run;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (this.Ps != PlayerState.Run) { Run(); }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
