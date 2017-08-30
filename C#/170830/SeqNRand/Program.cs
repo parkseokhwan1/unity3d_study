e15e2a3a40dcddd6a6b515a14b5f4d5d52544a95
parkseokhwan1 <kermian@naver.com>
2017-08-30 17:17:04 +0900

    ..

---
+++ C#/170830/SeqNRand/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+
+namespace SeqNRand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream outSteam = new FileStream("a.dat", FileMode.Create);
+            Console.WriteLine("Position : {0}", outSteam.Position);
+
+            outSteam.WriteByte(0x01);
+            Console.WriteLine("Position : {0}", outSteam.Position);
+
+            outSteam.WriteByte(0x02);
+            Console.WriteLine("Position : {0}", outSteam.Position);
+
+            outSteam.WriteByte(0x03);
+            Console.WriteLine("Position : {0}", outSteam.Position);
+
+            outSteam.Seek(5, SeekOrigin.Current);
+            Console.WriteLine("Position : {0}", outSteam.Position);
+
+            outSteam.WriteByte(0x04);
+            Console.WriteLine("Position : {0}", outSteam.Position);
+
+            outSteam.Close();
+        }
+    }
+}
