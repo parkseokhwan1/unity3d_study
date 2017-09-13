0cc9ee6e49562295440e20ca3fe49c45f7fa7778
parkseokhwan1 <kermian@naver.com>
2017-09-13 14:25:53 +0900

    ..

---
+++ Unity/workspace/Char_Ani_Test_01/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    float xRkey, ykey;
+    public GameObject ch;
+
+    Animator animator;
+    void Start () {
+        animator = ch.GetComponent<Animator>();
+    }
+	
+	void Update () {
+        xRkey = Input.GetAxis("Horizontal");
+        ykey = Input.GetAxis("Vertical");
+        print(ykey);
+
+        animator.SetFloat("Speed", Mathf.Abs(ykey));        // ykey의 절대값
+        this.transform.Translate(Vector3.forward * Time.deltaTime * ykey * 2.0f);
+        this.transform.Rotate(Vector3.up * Time.deltaTime * xRkey * 120f);
+    }
+}
